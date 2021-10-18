a9a66ea24e8db9542d6282a98173d03c6caaf753
cihat <cihat02020202@gmail.com>
2021-10-18 17:13:27 +0300

    Add footer button

---
+++ src/components/Buttons/FooterButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <button>Hizmet Kodu</button>
+</template>
+
+<script>
+export default {
+  name: "FooterButton",
+};
+</script>
+
+<style lang="scss" scoped>
+button {
+  padding: 10px 15px;
+  border: 1px solid #808080;
+  font-size: 13px;
+  line-height: 16.6px;
+  font-weight: 400;
+  color: #808080;
+  background-color: transparent;
+  cursor: pointer;
+  transition: all 0.4s ease;
+
+  &:hover {
+    background-color: #f5f5f5;
+    text-decoration: underline;
+  }
+}
+</style>
