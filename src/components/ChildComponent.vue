9ef0d1c3ce8ecc0733ba9643257e2b5b6eae25f9
cihat <cihat02020202@gmail.com>
2021-10-25 15:56:24 +0300

    Add ChildComponent

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: "ChildComponent",
+  props: {
+    test_prop: {
+      type: [Number, String],
+      default: "Hellooo",
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>{{ this.test_prop }}</h1>
+  </div>
+</template>
+
+<style>
+</style>
